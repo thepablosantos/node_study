d279802172072c789f0c1092f10f15be10ac1b29
Pablo Santos <iphonepablosantos@gmail.com>
2025-02-05 23:03:39 +0000

    att

---
+++ aula1/mode1.js
@@ -0,0 +1,12 @@
+const nome = 'Pablo';
+const sobrenome = 'Santos';
+
+const falaNome = () => {
+    console.log(nome, sobrenome);
+};
+
+exports.nome = nome;
+exports.sobrenome = sobrenome;
+exports.falaNome = falaNome;
+
+console.log(exports);
